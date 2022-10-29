4dfa81df7416f0a01979e6154d08a38ceaf459a4
kenya <reo_reo_reo_orz@yahoo.co.jp>
2022-10-30 04:53:25 +0900

    実績データ共通化

--- components/home/ProductSection.tsx
+++ components/home/ProductSection.tsx
@@ -2,83 +2,20 @@ import { css } from "@emotion/react";
 import ProductCard from "./ProductCard";
 import ReadMore from "../common/ReadMore";
 import { mqLarge } from "../../utils/style";
+import { ProductData } from "../../const/ProductData";
+import { productData } from "../../const/ProductData";
 
-type Product = {
-  pid: string;
-  imageUrl: { list: string; detail: string };
-  title: string;
-  brief: string;
-  category: string;
-  description: string;
-};
-
-let products: Product[] = [
-  {
-    pid: "1",
-    imageUrl: { list: "infanoid/list.jpg", detail: "infanoid/detail.jpg" },
-    title: "人間型ロボット「インファノイド」",
-    brief:
-      "関節は人間とほぼ同じ可動範囲で、頭、眉、眼、唇の動きで表情が作れる。",
-    category: "robot",
-    description:
-      "体高60cm、重量14kg、稼働箇所は28カ所。\n各関節は人間とほぼ同じ可動範囲であり、頭、眉、眼、唇の独立した動きは人間の表情を柔軟に表現する。\n目に内蔵されたカメラにより視線上にある物体の動きを追従することができ、「あっち向いてホイ」でインファノイドと対戦することもできる。\nワイヤにより駆動力を伝達し、軽量化とともにスムーズな動作を実現した。情報通信研究機構と当社の共同製作。",
-  },
-  {
-    pid: "2",
-    imageUrl: {
-      list: "ringOuhukuSouchi/list.jpg",
-      detail: "ringOuhukuSouchi/detail.jpg",
-    },
-    title: "リング往復回転装置",
-    brief: "リング部品を規定回数、規定時間往復回転させる装置。",
-    category: "automachine",
-    description: "",
-  },
-  {
-    pid: "3",
-    imageUrl: {
-      list: "tableIdouSouchi/list.jpg",
-      detail: "tableIdouSouchi/detail.jpg",
-    },
-    title: "大型テーブル移動装置",
-    brief:
-      " 900mm x 1800mmのテーブルを2000mm往復させる。サーボモータとボールネジで駆動。",
-    category: "experiment",
-    description: "",
-  },
-  {
-    pid: "4",
-    imageUrl: { list: "yumerobo/list.jpg", detail: "yumerobo/detail.jpg" },
-    title: "壁面昇降ロボット「夢ロボ」",
-    brief:
-      "垂直の壁を昇降することができる人型のロボット。上海万博に展示された。",
-    category: "robot",
-    description: "",
-  },
-  {
-    pid: "5",
-    imageUrl: {
-      list: "pushKensaSouchi/list.jpg",
-      detail: "pushKensaSouchi/detail.jpg",
-    },
-    title: "タッチパネル多点プッシュ検査装置",
-    brief:
-      "タッチ信号の正常な出力を検査するため、スクリーンの任意の点を正確かつ高速にプッシュする装置。",
-    category: "inspection",
-    description: "",
-  },
-  {
-    pid: "11",
-    imageUrl: {
-      list: "shiseiSeigyoSouchi/list.jpg",
-      detail: "shiseiSeigyoSouchi/detail.jpg",
-    },
-    title: "3軸姿勢制御装置",
-    brief: "ここに製品の概要が入ります。ここに製品の概要が入ります。",
-    category: "automachine",
-    description: "",
-  },
-];
+const targetProducts = productData.filter((product) => {
+  switch (product.pid) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "11":
+      return true;
+  }
+});
 
 const ProductSection = () => {
   return (
@@ -90,7 +27,7 @@ const ProductSection = () => {
           </div>
 
           <div css={cardWrapper}>
-            {products.map((product: Product, index: number) => {
+            {targetProducts.map((product: ProductData, index: number) => {
               return (
                 <ProductCard
                   pid={product.pid}
