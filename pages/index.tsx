c34cfc477accf4e47fbda133eeae445c6e22cffa
kenya <reo_reo_reo_orz@yahoo.co.jp>
2022-11-09 05:02:30 +0900

    カスタムフック名前修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,28 +11,7 @@ import type { NextPageWithLayout } from "./_app";
 import ServiceSection from "../components/pages/home/ServiceSection";
 
 const HomePage: NextPageWithLayout = () => {
-  const setViewportHeight = () => {
-    useEffect(() => {
-      const setViewportHeight = () => {
-        const vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
-      };
-
-      // 画面の横幅にサイズ変動がある場合、高さを再取得
-      let viewWidth = window.innerWidth;
-      window.addEventListener("resize", () => {
-        if (viewWidth === window.innerWidth) {
-          return;
-        }
-        viewWidth = window.innerWidth;
-        setViewportHeight();
-      });
-      setViewportHeight();
-      console.log("画面リサイズ");
-    }, []);
-  };
-
-  setViewportHeight();
+  useFillViewportHeight();
 
   return (
     <>
@@ -63,6 +42,26 @@ HomePage.getLayout = function getLayout(page: ReactElement) {
       </div>
     </>
   );
+};
+const useFillViewportHeight = () => {
+  useEffect(() => {
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    // 画面の横幅にサイズ変動がある場合、高さを再取得
+    let viewWidth = window.innerWidth;
+    window.addEventListener("resize", () => {
+      if (viewWidth === window.innerWidth) {
+        return;
+      }
+      viewWidth = window.innerWidth;
+      setViewportHeight();
+    });
+    setViewportHeight();
+    console.log("画面リサイズ");
+  }, []);
 };
 
 const wrapper = css`
